a03b58071743eeed05d77b876c1c9317d83c3913
AmanSinghThakur01 <thakuramansinghh01@gmail.com>
2025-10-27 23:40:32 +0530

    responsive function created for mobilee or other devices

---
+++ lib/screens/portfolio.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+class Portfolio extends StatefulWidget {
+  const Portfolio({super.key});
+
+  @override
+  State<Portfolio> createState() => _PortfolioState();
+}
+
+class _PortfolioState extends State<Portfolio> {
+  List<Widget> navItems = [
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(onPressed: (){}, child: Row(children: [Text("Education",),Icon(Icons.school),],),),
+    ),
+    Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ElevatedButton(onPressed: (){}, child: Row(children: [Text("Skills",),Icon(Icons.code),],),),
+    ),
+  ];
+  bool isMobile = false ;
+  @override
+  Widget build(BuildContext context) {
+    isMobile = MediaQuery
+        .of(context)
+        .size
+        .width > 700 ? false : true;
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Aman Singh Thakur"),
+          actions: isMobile ? null : navItems,
+        ),
+        drawer: isMobile ?
+        Drawer(
+          child: ListView(
+            children: navItems,
+          ),
+        ) : null,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Wrap(
+              children: [],
+            ),
+          ],
+        ),
+      )
+    );
+
+  }
+}
